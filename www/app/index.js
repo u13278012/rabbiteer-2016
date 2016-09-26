f5cecd2d201317953202bfd13b5d7b3e79482735
Cillié Malan <cilliemalan@gmail.com>
2016-09-26 17:42:46 +0200

    Added index.js (clientside)

---
+++ www/app/index.js
@@ -0,0 +1,6 @@
+//This is the entry point for our clientside angular app
+const angular = require('angular');
+const angularLoadingBar = require('angular-loading-bar');
+const angularUiRouter = require('angular-ui-router');
+
+const app = angular.module('app', [angularLoadingBar, angularUiRouter]);
