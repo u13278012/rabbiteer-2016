379a1152cfde2d1e8e5666c86ca22730ca47d164
Cillié Malan <cilliemalan@gmail.com>
2016-09-26 16:20:05 +0200

    opn-ing the web page when in dev environment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 
 import {Server} from './server';
+import opn from 'opn';
 
 
 // create a new instance of our server class
@@ -8,9 +9,13 @@ let server = new Server();
 // start listening
 server.listen().then(() => {
   console.log(`listening on port ${server.port}`);
+  if (process.env.NODE_ENV === 'development') {
+    opn(server.url);
+  }
+
 }, e => {
   console.error(e.message)
-  if(e.code === "EADDRINUSE") {
+  if (e.code === "EADDRINUSE") {
     console.log('the port/address is already in use. Is another instance already running?')
   }
 });
