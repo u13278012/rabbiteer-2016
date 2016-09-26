ab50447f4271cdd9887893b9413dd2a2c9b6038a
Cillié Malan <cilliemalan@gmail.com>
2016-09-26 10:37:51 +0200

    Managed to get everything working again with new deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,17 @@
 var gulp = require('gulp-help')(require('gulp'));
 var eslint = require('gulp-eslint');
 var mocha = require('gulp-mocha');
-var istanbul = require('gulp-istanbul');
 var jsdoc = require('gulp-jsdoc3');
 var sequence = require('run-sequence');
 var babel = require('gulp-babel');
-var isparta = require('isparta');
-var babelRegister = require('babel/register');
+var babelRegister = require('babel-core/register');
+var spawn = require('child_process').spawn;
+var path = require('path');
 
 var GULP_FILE = ['gulpfile.js'];
 var SRC_FILES = ['src/**/*.js'];
 var TEST_FILES = ['test/**/*.js'];
-var TEST_CASE_FILES = ['test/**/*.test.js'];
-var COVERAGE_REPORT_DIR = 'build/coverage';
+var TEST_CASE_FILES = ['test/**/*.spec.js'];
 var COMPILED_SRC_DIR = 'build/source';
 var JSDOC_DIR = 'build/jsdoc';
 
@@ -24,26 +23,29 @@ gulp.task('lint', 'Validates code with "eslint"', function (done) {
     .on('finish', done);
 });
 
-gulp.task('test', 'Runs tests and generates code coverage report', function (done) {
-  gulp.src(SRC_FILES)
-    .pipe(istanbul({
-      instrumenter: isparta.Instrumenter,
-      includeUntested: true
-    }))
-    .pipe(istanbul.hookRequire())
-    .on('finish', function () {
-      gulp.src(TEST_CASE_FILES)
-        .pipe(mocha({compilers: {js: babelRegister}}))
-        .pipe(istanbul.writeReports({
-          dir: COVERAGE_REPORT_DIR
-        }))
-        .pipe(istanbul.enforceThresholds({
-          thresholds: {
-            global: 100
-          }
-        }))
-        .on('finish', done);
-    });
+gulp.task('test', 'Run tests without code coverage', function () {
+  return gulp.src(TEST_CASE_FILES)
+    .pipe(mocha({ compilers: { js: babelRegister } }));
+});
+
+gulp.task('coverage', 'Runs tests and generates code coverage report', function (done) {
+  console.log('running code coverage...');
+
+  let file = 'nyc';
+  if (process.platform === 'win32') file = 'nyc.cmd';
+
+  let execute = () => {
+    return spawn(path.join(__dirname, 'node_modules/.bin/', file), ['node_modules/.bin/mocha'], { stdio: "inherit", cwd: __dirname });
+  }
+
+  execute().on('close', (code) => {
+    if (code === 1) {
+      //run again just in case
+      execute().on('close', done);
+    } else {
+      done(code);
+    }
+  });
 });
 
 gulp.task('compile', 'Compiles source code from es6 to es5', function (done) {
@@ -54,17 +56,14 @@ gulp.task('compile', 'Compiles source code from es6 to es5', function (done) {
 });
 
 gulp.task('jsdoc', 'Generates jsdoc', ['compile'], function (done) {
-  gulp.src(SRC_FILES, {read: false})
+  gulp.src(SRC_FILES, { read: false })
     .pipe(jsdoc({
-      opts: {destination: JSDOC_DIR},
-      templates: {
-        theme: 'cerulean'
-      }
+      opts: { destination: JSDOC_DIR }
     }, done));
 });
 
 gulp.task('build', 'Builds source code: validates it and provides an artifacts', function (done) {
-  sequence('lint', 'test', 'compile', 'jsdoc', done);
+  sequence('lint', 'coverage', 'compile', 'jsdoc', done);
 });
 
 gulp.task('pre-commit', 'Being run automatically on a git pre-commit hook', ['build']);
