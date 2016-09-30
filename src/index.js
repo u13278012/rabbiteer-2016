ff1f1e3cc72b06270dc48b4103e0abe32d7256d0
Cillié Malan <cilliemalan@gmail.com>
2016-09-30 08:19:45 +0200

    Added some comments for src/index

--- src/index.js
+++ src/index.js
@@ -1,31 +1,34 @@
 
 import {Server} from './server';
-import opn from 'opn';
 import {startDebugSupport} from './debugsupport';
 
-
+//figure out what environment we are in (either development or production).
+// if we are in a dev environment we enable more debug features.
 const development = process.env.NODE_ENV !== 'production';
 
-if(development) {
+if (development) {
   startDebugSupport();
 }
 
 // create a new instance of our server class
 let server = new Server({
+  //where static files will be served from
   staticroot: "www"
 });
 
 // start listening
 server.listen().then(() => {
-  let message = `listening really hard on port ${server.port}`;
-  console.log(message);
+  console.log(`listening on port ${server.port}`);
   if (development) {
-    opn(server.url);
+    //this opens the browser
+    require('opn')(server.url);
   }
-
 }, e => {
+  //oh noes!
   console.error(e.message)
   if (e.code === "EADDRINUSE") {
     console.log('the port/address is already in use. Is another instance already running?')
   }
 });
+
+//the program will continue running while the server is listening.
