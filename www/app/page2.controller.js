827a0ef24687b0d5816829b3c5e7e64818985e0a
Cillié Malan <cilliemalan@gmail.com>
2016-10-03 21:12:29 +0200

    Linted

--- www/app/page2.controller.js
+++ www/app/page2.controller.js
@@ -1,6 +1,6 @@
-module.exports = function($scope, $interval) {
-  
-  
+module.exports = function ($scope, $interval) {
+
+
   $scope.date = Date.now();
 
   //formats
@@ -9,21 +9,20 @@ module.exports = function($scope, $interval) {
   formats.date = 'yyyy/MM/dd';
   formats.time = 'HH:mm:ss a';
 
-
-  function tick(clock) {
+  function tick() {
     $scope.date = Date.now();
   }
 
-  var initClock = function() {
+  var initClock = function () {
     $interval(tick, 1000);
   };
 
   $scope.clocks = [
     {
       format: formats.normal
-    },{
+    }, {
       format: formats.date
-    },{
+    }, {
       format: formats.time
     }
   ];
