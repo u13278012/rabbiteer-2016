7b5daae38ffd0485bd49bac359d762a91e4a6cdc
Cillié Malan <cilliemalan@gmail.com>
2016-10-03 21:02:46 +0200

    Fixed problems from previous commit

--- www/app/page3.controller.js
+++ www/app/page3.controller.js
@@ -1,29 +1,31 @@
-module.exports = function($scope) {
-    $scope.todoList = [
-      {text:'learn angular', done:true},
-      {text:'build an angular app', done:false}];
- 
-    $scope.todoList.addTodo = function() {
-      $scope.todoList.push({text:$scope.todoList.todoText, done:false});
-      $scope.todoList.todoText = '';
-      console.log($scope.todoList);
-    };
- 
-    $scope.todoList.remaining = function() {
-      var count = 0;
-      angular.forEach($scope.todoList, function(todo) {
-        count += todo.done ? 0 : 1;
-      });
-      return count;
-    };
- 
-    $scope.todoList.archive = function() {
-      var oldTodos = $scope.todoList;
-      $scope.todoList = [];
-      angular.forEach(oldTodos, function(todo) {
-        if (!todo.done) {
-          $scope.todoList.push(todo);
-        }
-      });
-    };
+const angular = require('angular');
+
+module.exports = function ($scope) {
+  $scope.todoList = [
+    { text: 'learn angular', done: true },
+    { text: 'build an angular app', done: false }];
+
+  $scope.todoList.addTodo = function () {
+    $scope.todoList.push({ text: $scope.todoList.todoText, done: false });
+    $scope.todoList.todoText = '';
+    console.log($scope.todoList);
+  };
+
+  $scope.todoList.remaining = function () {
+    var count = 0;
+    angular.forEach($scope.todoList, function (todo) {
+      count += todo.done ? 0 : 1;
+    });
+    return count;
+  };
+
+  $scope.todoList.archive = function () {
+    var oldTodos = $scope.todoList;
+    $scope.todoList = [];
+    angular.forEach(oldTodos, function (todo) {
+      if (!todo.done) {
+        $scope.todoList.push(todo);
+      }
+    });
+  };
 }
