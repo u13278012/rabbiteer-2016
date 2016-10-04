3655998ebd0d82424f6db510b5c6538cc8749682
DanielKeun <dkeun@retrorabbit.co.za>
2016-10-04 15:06:49 +0200

    added some example css and js things

--- www/app/devtools.controller.js
+++ www/app/devtools.controller.js
@@ -1,7 +1,19 @@
-module.exports = function($scope) {
+module.exports = function ($scope) {
   //$scope.inputList = [];
-  $scope.example2 = function(){
-    
+  $scope.example2 = function () {
+
     $scope.inputList.push($scope.inputText);
+
+  }
+  
+  $scope.debugFunction = function () {
+    debugger;
+
+    var a = 1;
+    console.log(a);
+
+    a = a + 1;
+
+    console.log(a);
   }
 }
