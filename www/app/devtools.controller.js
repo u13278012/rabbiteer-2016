6b45e7c9138da4523d91997b69841947911c0dc9
Merrick Kavolsky <technicmk@gmail.com>
2016-10-05 18:05:07 +0200

    Fix devtools.controller bug

--- www/app/devtools.controller.js
+++ www/app/devtools.controller.js
@@ -1,5 +1,5 @@
 module.exports = function ($scope) {
-  //$scope.inputList = [];
+  $scope.inputList = [];
   $scope.example2 = function () {
 
     $scope.inputList.push($scope.inputText);
@@ -11,9 +11,7 @@ module.exports = function ($scope) {
 
     var a = 1;
     console.log(a);
-
     a = a + 1;
-
     console.log(a);
   }
 }
