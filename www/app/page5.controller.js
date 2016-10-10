9eefbbece2b2dc8a212126b66df6768b6f81ef6e
Werner Scheffer <wscheffer@retorabbit.co.za>
2016-10-10 21:09:05 +0200

    Added Basic Firebase Chat with Google and GitHub login

--- www/app/page5.controller.js
+++ www/app/page5.controller.js
@@ -1,45 +1,77 @@
-var firebase = require("firebase");
+const firebase = require("firebase");
 
 module.exports = function ($scope) {
-  $scope.nick = "hobp"
-  $scope.location = window.location.hostname 
+
+  $scope.nick = "Anon"
+  $scope.location = window.location.hostname
   $scope.messages = [];
   $scope.newMessage = {};
-
   var config = {
     apiKey: "AIzaSyBnRqWBvsbR-5bnvaRAZZbJc_DQK2JzxHo",
     authDomain: "rabbiteer-bbb91.firebaseapp.com",
     databaseURL: "https://rabbiteer-bbb91.firebaseio.com",
     storageBucket: "rabbiteer-bbb91.appspot.com"
   };
-  firebase.initializeApp(config);
 
-  // Listen for authentication state changes
-  firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-      // If the user is logged in, set them as the Firechat user
-      $scope.nick += user.uid.substr(10, 8)
-    } else {
-      // If the user is not logged in, sign them in anonymously
-      firebase.auth().signInAnonymously().catch(function (error) {
-        console.log("Error signing user in anonymously:", error);
-      });
+  var providerGitHub = new firebase.auth.GithubAuthProvider();
+  var providerGoogle = new firebase.auth.GoogleAuthProvider();
+
+  if (firebase.apps.length == 0)
+    firebase.initializeApp(config);
+
+  firebase.auth().getRedirectResult().then(function (result) {
+    if (result.credential) {
+      // This gives you Selected Provider's Access Token. You can use it to access the APIs of the selected provider.
+      //var token = result.credential.accessToken;
+
     }
+    // The signed-in user info.
+  }).catch(function (/*error*/) {
+    // Handle Errors here.
+    ////var errorCode = error.code;
+    ////var errorMessage = error.message;
+    // The email of the user's account used.
+    ////var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    ////var credential = error.credential;
+    // ...
   });
 
-  var ref = '/chat_rooms/' + $scope.location+ '/messages'
+  var ref = '/chat_rooms/' + $scope.location + '/messages'
   var messagesdbRef = firebase.database().ref(ref);
 
   messagesdbRef.on('child_added', function (data) {
     let message = data.val()
+
     $scope.messages.push({
-      current: $scope.nick == message.nick ? true : false,
+      id: data.key,
+      current: message.userid == $scope.user.uid ? true : false,
       nick: message.nick,
       message: message.message
     });
-
     $scope.$apply();
   });
+
+  // Listen for authentication state changes
+  firebase.auth().onAuthStateChanged(function (user) {
+
+    if (user) {
+      // If the user is logged in, set the nickname and store the Firebase User for later use 
+      $scope.nick = user.displayName;
+      $scope.user = user;
+      $scope.$apply();
+    }
+  });
+
+  $scope.signInWithGitHub = function () {
+    // Sign them in with GitHub
+    firebase.auth().signInWithRedirect(providerGitHub);
+  }
+
+  $scope.signInWithGoogle = function () {
+    // Sign them in with GitHub
+    firebase.auth().signInWithRedirect(providerGoogle);
+  }
 
   $scope.RandomCode = function () {
     var today = new Date();
@@ -52,11 +84,49 @@ module.exports = function ($scope) {
     return "" + mm + dd + hh + mm + ss + ran;
   };
 
+  $scope.isLoggedIn = function () {
+    return $scope.user != undefined;
+  }
+
+  $scope.signOut = function () {
+    firebase.auth().signOut();
+    $scope.user = undefined;
+  }
+
+  $scope.changeDisplayName = function (displayname) {
+    $scope.user.updateProfile({
+      displayName: displayname
+    }).then(function () {
+      // Profile updated successfully!
+      var displayName = $scope.user.displayName;
+      $scope.nick = displayName;
+      alert('Name successfully changed to ' + displayName);
+    }, function (/*error*/) {
+      // An error happened.
+    });
+  }
+
+  $scope.changeDisplayNameOnEnter = function (event, displayname) {
+    if (event.key === "Enter") {
+      $scope.changeDisplayName(displayname);
+    }
+  }
+
   $scope.sendMessage = function (newMessage) {
-    debugger
     firebase.database().ref(ref + '/' + $scope.RandomCode()).set({
+      userid: $scope.user.uid,
       nick: $scope.nick,
       message: newMessage.message
+    }, function (e) {
+      if (e != null)
+        alert(e.message);
     });
+    $scope.newMessage.message = "";
+  }
+  
+  $scope.sendMessageOnEnter = function (event, newMessage) {
+    if (event.key === "Enter") {
+      $scope.sendMessage(newMessage);
+    }
   }
 }
